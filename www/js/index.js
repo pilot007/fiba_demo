d57708f17f367fd9c2f6813be75c3ba58f25b229
pilot007 <selimgoktas@gmail.com>
2016-06-21 14:50:32 +0300

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -72,71 +72,7 @@ var pieData = [
 
 }
 ];
-/*
-function initPushwoosh() {
-	var pushNotification = window.plugins.pushNotification;
-	if(device.platform == "Android")
-	{
-		registerPushwooshAndroid();
-	}
 
-	if(device.platform == "iPhone" || device.platform == "iOS")
-	{
-		registerPushwooshIOS();
-	}
-}*/
-function initPushwoosh() {
-	
-	var pushNotification = window.plugins.pushNotification;
-	
-	//set push notification callback before we initialize the plugin
-	document.addEventListener('push-notification', function(event) {
-								//get the notification payload
-								var notification = event.notification;
-
-								//display alert to the user for example
-								alert(notification.aps.alert);
-							  
-								//clear the app badge
-								pushNotification.setApplicationIconBadgeNumber(0);
-							});
-
-	
-    //initialize the plugin
-    pushNotification.onDeviceReady({pw_appid:"B9CE7-9D257"});
-
-    //register for pushes
-	pushNotification.registerDevice(function(status) {
-                                        var deviceToken = status['deviceToken'];
-                                        console.warn('registerDevice: ' + deviceToken);
-									},
-									function(status) {
-                                        console.warn('failed to register : ' + JSON.stringify(status));
-                                        navigator.notification.alert(JSON.stringify(['failed to register ', status]));
-									});
-    
-	pushNotification.setApplicationIconBadgeNumber(0);
-    
-	pushNotification.getTags(function(tags) {
-								console.warn('tags for the device: ' + JSON.stringify(tags));
-							 },
-							 function(error) {
-								console.warn('get tags error: ' + JSON.stringify(error));
-							 });
-
-	pushNotification.getPushToken(function(token) {
-								  console.warn('push token device: ' + token);
-							 });
-
-	pushNotification.getPushwooshHWID(function(token) {
-									console.warn('Pushwoosh HWID: ' + token);
-								});
-
-	//start geo tracking.
-    pushNotification.startLocationTracking(function() {
-                                           console.warn('Location Tracking Started');
-                                           });
-}
 var app = {
 	// Application Constructor
 	initialize : function() {
@@ -159,7 +95,6 @@ var app = {
 	},
 	onDeviceReady : function() {
 		console.log("ondevice ready");
-		initPushwoosh();
 		app.receivedEvent('deviceready');
 		app.first_init();
 		
